947f84fa8675ec54b6c2d44fbce2121190458c25
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-03-25 07:48:03 +0800

    入口文件

--- src/DouYin.php
+++ src/DouYin.php
@@ -2,22 +2,23 @@
 /**
  * GitHub:https://github.com/SouthWind017/
  * Name:zhang tian yu
- * CreateTime:2021/3/25 0025 05:48
+ * CreateTime:2021/3/25 0025 06:44
  * IdeName:PhpStorm
  * FileName:douyin-php-sdk
  * @copyright (c) douyin-php-sdk All Rights Reserved
  */
 
-namespace Douyin;
-
-
-class Douyin {
+class Douyin{
 
     public static function __callStatic($name , $arguments)
     {
-
-        $name = ucfirst(strtolower($name)); //类名重定义
-        var_dump($name);
+        $name = ucfirst(strtolower($name));
+        $class = "\\Base\\{$name}";
+        if (!empty($class) && class_exists($class)) {
+            $option = array_shift($arguments);
+            $config =  $option;
+            return new $class($config);
+        }
+        return "找不到对应的函数或类";
     }
-
 }
