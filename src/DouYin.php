29998ce21af7ee67162ca74130e9ce0802355711
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-03-25 04:05:55 +0800

    入口文件

---
+++ src/DouYin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * GitHub:https://github.com/SouthWind017/
+ * Name:zhang tian yu
+ * CreateTime:2021/3/25 0025 03:59
+ * IdeName:PhpStorm
+ * FileName:douyin-php-sdk
+ * @copyright (c) PhpStorm All Rights Reserved
+ */
+
+namespace Douyin;
+
+use ByteDance\Kernel\DataArray;
