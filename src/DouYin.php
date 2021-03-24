78abe95d1ec618a13baf5a4f10a64881ec856704
SouthWind017 <65751313+SouthWind017@users.noreply.github.com>
2021-03-25 06:22:09 +0800

    main

--- src/DouYin.php
+++ src/DouYin.php
@@ -2,12 +2,22 @@
 /**
  * GitHub:https://github.com/SouthWind017/
  * Name:zhang tian yu
- * CreateTime:2021/3/25 0025 03:59
+ * CreateTime:2021/3/25 0025 05:48
  * IdeName:PhpStorm
  * FileName:douyin-php-sdk
- * @copyright (c) PhpStorm All Rights Reserved
+ * @copyright (c) douyin-php-sdk All Rights Reserved
  */
 
 namespace Douyin;
 
-use ByteDance\Kernel\DataArray;
+
+class Douyin {
+
+    public static function __callStatic($name , $arguments)
+    {
+
+        $name = ucfirst(strtolower($name)); //类名重定义
+        var_dump($name);
+    }
+
+}
